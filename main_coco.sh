c9d146a546884fdc3d34f69b283a2df919b54c58
lukaszbinden <lukas.zbinden@unifr.ch>
2018-11-02 17:09:25 +0100

    to run for MS COCO dataset

---
+++ main_coco.sh
@@ -0,0 +1,24 @@
+# Copyright (c) 2017-present, Facebook, Inc.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
+#!/bin/bash
+
+DIR="coco/2017_training/clust_imgs_224x224"
+ARCH="alexnet"
+LR=0.05
+WD=-5
+K=3000
+PRE_MODEL="deepcluster_models/alexnet/checkpoint.pth.tar"
+WORKERS=1
+BATCH_SIZE=1
+EXP="main_coco_out"
+PYTHON="/home/lz01a008/.conda/envs/faiss/bin/python"
+
+mkdir -p ${EXP}
+
+CUDA_VISIBLE_DEVICES=0 ${PYTHON} main.py ${DIR} --exp ${EXP} --arch ${ARCH} \
+  --resume ${PRE_MODEL} --batch ${BATCH_SIZE} \
+  --lr ${LR} --wd ${WD} --k ${K} --sobel --verbose --workers ${WORKERS}
